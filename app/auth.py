8055352e8ea1359c06fb7f5596ba88dc9d04b7fe
KarimLak <karim.lakhal@polymtl.ca>
2026-05-07 20:02:03 -0400

    token functions

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,11 @@
+import datetime
+import os
+from dotenv import load_dotenv
+from fastapi import HTTPException
 from passlib.context import CryptContext
+from jose import jwt, JWTError
 
+load_dotenv() 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
 def hash_password(password: str) -> str:
@@ -7,3 +13,19 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
+
+def create_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + datetime.timedelta(minutes=os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, os.getenv('SECRET_KEY'), os.getenv('ALGORITHM'))
+
+def verify_token(token: str) -> str:
+    try:
+        payload = jwt.decode(token, os.getenv('SECRET_KEY'), os.getenv('ALGORITHM'))
+        username = payload.get("sub")
+        if not username:
+            raise HTTPException(status_code=401, detail= "Invalid token")
+        return username
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
