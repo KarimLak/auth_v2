7b7af3b950eb562cba90fab8136eb41544c227a2
KarimLak <karim.lakhal@polymtl.ca>
2026-05-21 11:23:40 -0400

    get contract service

--- app/services/contract.py
+++ app/services/contract.py
@@ -1,9 +1,23 @@
+from sqlalchemy import or_, select
 from typing import List
 
 from sqlalchemy.orm import Session
 
 
+from app.models.contract import Contract
 from app.schemas.contract import ContractFilter, ContractResponse
 
 def get_contracts(payload: ContractFilter, skip: int, limit: int, db: Session) -> List[ContractResponse]:
-    return 1
+    data = payload.model_dump(exclude_none=True)
+    filter_contracts = select(Contract)
+    for k, i in data.items():
+        column = getattr(Contract, k, None)
+
+        if column is not None:
+            filter_contracts = filter_contracts.where(column==i)
+
+    query = filter_contracts.offset(skip).limit(limit)
+    contracts = db.execute(query).scalars().all()
+    return contracts
+
+
